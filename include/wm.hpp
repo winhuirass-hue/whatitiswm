420ba6f2ed30a2c804217390fe5771515c64cb87
winhuirass-hue <winhuirass@gmail.com>
2026-03-15 20:34:28 +0200

    Update wm.hpp

--- include/wm.hpp
+++ include/wm.hpp
@@ -6,6 +6,8 @@
 #include <X11/extensions/Xcomposite.h>
 #include <X11/extensions/Xdamage.h>
 #include <X11/extensions/Xrender.h>
+#include <X11/extensions/Xfixes.h>
+#include <X11/extensions/shape.h> 
 #include <GL/glx.h>
 #include <GL/gl.h>
 
